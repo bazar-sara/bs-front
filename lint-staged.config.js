91891f2cdd652e6ff7fa4adae0c69c29257c90a1
shayan-hamidi <sha.hamidi10@gmail.com>
2025-10-24 01:18:00 +0330

    feat: project init

---
+++ lint-staged.config.js
@@ -0,0 +1,16 @@
+export default {
+  '**/*.{js,jsx,ts,tsx}': (filenames) => [
+    `npx eslint --fix ${filenames
+      .map((filename) => `"${filename}"`)
+      .join(' ')}`,
+  ],
+  '**/*.(md|json)': (filenames) =>
+    `npx prettier --write ${filenames
+      .map((filename) => `"${filename}"`)
+      .join(' ')}`,
+  'src/i18n/*.json': (filenames) => [
+    `npx eslint --fix ${filenames
+      .map((filename) => `"${filename}"`)
+      .join(' ')}`,
+  ],
+};
